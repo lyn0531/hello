7267f5de34ee29c9fd1b81f66c71e8f259905559
lyn0531 <47732410+lyn0531@users.noreply.github.com>
2019-02-18 16:33:43 +0900

    Rename cds_led.ino to ch4조도센서.ino

---
+++ ch4조도센서.ino
@@ -0,0 +1,18 @@
+int led = 3;
+int cds = A0;
+
+void setup() {
+  pinMode(led, OUTPUT); 
+}
+
+void loop() {
+  int light = analogRead(cds); 
+
+  if (light < 600) {
+    digitalWrite(led, HIGH);
+  }
+
+  else {
+    digitalWrite(led, LOW);
+  }
+}
